a5d760115fb3d4c6eb236356754c81766d4366de
RAVI GANDHI <ravi11294@yahoo.in>
2018-06-06 12:00:40 +0530

    control layer CrimeFragments(inflator)

---
+++ app/src/main/java/CrimeFragment.java
@@ -0,0 +1,27 @@
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.knoxpo.khyati.criminalintent.Crime;
+import com.knoxpo.khyati.criminalintent.R;
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View v = inflater.inflate(R.layout.fragment_crime,container, false);
+        return v;
+    }
+}
