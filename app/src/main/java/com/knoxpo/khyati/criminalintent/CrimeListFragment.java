aec176745355276ef2758a0ddd82d8b38a10de71
RAVI GANDHI <ravi11294@yahoo.in>
2018-06-08 12:11:52 +0530

    binding viewholder & adapter

--- app/src/main/java/com/knoxpo/khyati/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/knoxpo/khyati/criminalintent/CrimeListFragment.java
@@ -1,12 +1,15 @@
 package com.knoxpo.khyati.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
 
 public class CrimeListFragment extends Fragment {
     private RecyclerView mCrimeRecyclerView;
@@ -19,10 +22,36 @@ public class CrimeListFragment extends Fragment {
         return  view;
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder{
-        public CrimeHolder(LayoutInflater inflater , ViewGroup parent){
+    private class CrimeHolder extends RecyclerView.ViewHolder
+    {
+        public CrimeHolder(LayoutInflater inflater , ViewGroup parent)
+        {
             super(inflater.inflate(R.layout.list_item_crime,parent,false));
 
+        }
+    }
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>
+    {
+        private List<Crime> mCrimes;
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @NonNull
+        @Override
+        public CrimeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new CrimeHolder(layoutInflater,parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CrimeHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
         }
     }
 
