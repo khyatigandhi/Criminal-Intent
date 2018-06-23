fb881044130708d67dc62ec73dd725e612fbc207
Khyati Gandhi <khyatigandhi1998@gmail.com>
2018-06-23 10:39:26 +0530

    retriving  extras

--- app/src/main/java/com/knoxpo/khyati/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/knoxpo/khyati/criminalintent/CrimeFragment.java
@@ -16,6 +16,8 @@ import android.widget.EditText;
 import com.knoxpo.khyati.criminalintent.Crime;
 import com.knoxpo.khyati.criminalintent.R;
 
+import java.util.UUID;
+
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
@@ -25,7 +27,8 @@ public class CrimeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mCrime = new Crime();
+        UUID crimeId = (UUID) getActivity().getIntent().getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
     @Override
@@ -33,6 +36,7 @@ public class CrimeFragment extends Fragment {
     {
         View v = inflater.inflate(R.layout.fragment_crime,container, false);
         mTitleField = v.findViewById(R.id.crime_title);
+        mTitleField.setText((CharSequence) mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -53,6 +57,7 @@ public class CrimeFragment extends Fragment {
         mDateButton.setText(mCrime.getDate().toString());
         mDateButton.setEnabled(false);
         mSolvedCheckBox = v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
