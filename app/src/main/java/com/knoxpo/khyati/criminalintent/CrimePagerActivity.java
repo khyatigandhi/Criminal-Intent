9f77b9ea349bf73e9615790776706d1d07257a17
Khyati Gandhi <khyatigandhi1998@gmail.com>
2018-06-25 17:34:21 +0530

    integrating crimepager activity

--- app/src/main/java/com/knoxpo/khyati/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/knoxpo/khyati/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.knoxpo.khyati.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -8,16 +10,26 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
+import java.util.UUID;
 
 public class CrimePagerActivity extends AppCompatActivity {
 
+    private static final String EXTRA_CRIME_ID = "crime_id";
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext , UUID crimeId){
+        Intent intent = new Intent(packageContext,CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID,crimeId);
+        return intent;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
 
         mViewPager = findViewById(R.id.crime_view_pager);
         mCrimes = CrimeLab.get(this).getCrimes();
